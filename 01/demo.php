2c153202ebbb995ae49ead382c9bf328f2e2ff75
ye422 <ye422@naver.com>
2023-08-03 14:14:25 +0900

    initial

---
+++ 01/demo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<h2>PHP로 반복 출력하기</h2>
+<?php
+	$i=1;
+	while($i<=5) {
+		echo "안녕하세요.<br>";
+		$i++;
+	}
+?> 
+</h1>
+</body>
+</html>
