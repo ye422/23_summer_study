8e9f1557139bd91176d66ff25f4e39320a7badd7
ye422 <ye422@naver.com>
2023-08-31 21:26:33 +0900

    chapter 11

---
+++ 10/ans.php
@@ -0,0 +1,33 @@
+mysqli_connect()
+
+htmlspecialchars()
+
+mysqli_query()
+
+mysqli_close()
+
+mysqli_fetch_assoc()
+
+htmlspecialchars()
+
+regist_day
+
+mysqli_connect()
+
+insert
+
+mysqli_query()
+
+$name
+$pass
+$subject
+$content
+
+$date
+select
+mysqli_query($con, $sql);
+
+delete from freeboard where num=$num
+$con
+$con
+location.href= 'list.php'
---
+++ 11/ans.php
@@ -0,0 +1,32 @@
+1. 
+$con = mysqli_connect();
+mysqli_query($con, $sql);
+mysqli_num_rows($result);
+
+if ( !$num_record )
+
+2.
+$id = $_POST["id"];
+$pass = $_POST["pass"];
+$name = $_POST["name"];
+$email = $_POST["email"];
+
+date("Y-m-d (H:i)");
+
+mysqli_query($con, $sql);
+
+3.
+where
+$result 
+$row = mysqli_num_rows($result);
+
+$pass != $db_pass
+
+$_SESSION["userid"] = $row["id"];
+$_SESSION["username"] = $row["name"];
+
+4.
+session_start();
+unset($_SESSION["userid"]);
+unset($_SESSION["username"]);
+
